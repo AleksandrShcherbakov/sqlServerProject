955e52bf1c63e46d21e2f79212f257a41ce889f1
AleksandrShcherbakov <shcherbakov89@gmail.com>
2019-02-14 18:14:14 +0300

    lastday

---
+++ src/main/java/com/work/sqlServerProject/controller/MainController.java
@@ -0,0 +1,25 @@
+package com.work.sqlServerProject.controller;
+
+import com.work.sqlServerProject.dao.CellNameDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by AlVlShcherbakov on 14.02.2019.
+ */
+@Controller
+public class MainController {
+
+    @Autowired
+    private CellNameDAO cellNameDAO;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String find(){
+        int id=13010;
+        return cellNameDAO.findCellNumber(id).toString();
+    }
+}
---
+++ src/main/java/com/work/sqlServerProject/dao/CellNameDAO.java
@@ -0,0 +1,37 @@
+package com.work.sqlServerProject.dao;
+
+import com.work.sqlServerProject.mapper.CellNameMapper;
+import com.work.sqlServerProject.model.CellNameInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by AlVlShcherbakov on 14.02.2019.
+ */
+
+@Repository
+public class CellNameDAO extends JdbcDaoSupport {
+
+    @Autowired
+    public CellNameDAO(DataSource dataSource) {
+        this.setDataSource(dataSource);
+    }
+
+    public CellNameInfo findCellNumber(int id){
+        String sql = CellNameMapper.BASE_SQL+" where CI="id";";
+        Object[] params = new Object[]{id};
+        CellNameMapper mapper = new CellNameMapper();
+        try {
+            CellNameInfo cellName = this.getJdbcTemplate().queryForObject(sql,params,mapper);
+            System.out.println(cellName.getCellName()+" "+cellName.getCI());
+            return cellName;
+        }
+        catch (EmptyResultDataAccessException e){
+            return null;
+        }
+    }
+}
